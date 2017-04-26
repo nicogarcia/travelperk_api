0d100df5b89834721b92313f12c085877564c4d8
nicogarcia <nicoalbo90@gmail.com>
2017-04-26 20:21:43 -0300

    Created production config

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "travelperk_project.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "travelperk_project.development.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
--- travelperk_project/wsgi_production.py
+++ travelperk_project/wsgi_production.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "travelperk_project.development.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "travelperk_project.production.settings")
 
 application = get_wsgi_application()
