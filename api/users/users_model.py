e0426feb44ba6bf2706bed676e9a162f02a01e1b
nicogarcia <nicoalbo90@gmail.com>
2017-04-26 21:16:36 -0300

    Fixed custom user

--- api/users/users_model.py
+++ api/users/users_model.py
@@ -1,5 +1,14 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.db import models
 
 
-class User(AbstractUser):
-    pass
+class User(AbstractBaseUser):
+    USERNAME_FIELD = 'email'
+
+    email = models.EmailField(unique=True)
+
+    def get_short_name(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.email
