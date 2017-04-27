2e25f5ba5aecd149c9a39958300de5e149930339
nicogarcia <nicoalbo90@gmail.com>
2017-04-27 09:33:10 -0300

    Added cors headers

--- travelperk_project/development/settings.py
+++ travelperk_project/development/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 APPEND_SLASH = False
 
 ALLOWED_HOSTS = []
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -38,12 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api.apps.ApiConfig'
+    'api.apps.ApiConfig',
+    'corsheaders',
 ]
 
 AUTH_USER_MODEL = 'api.User'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
