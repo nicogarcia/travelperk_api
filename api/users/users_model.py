905f5d117afdcd1f9c9e905239427fa4748576fa
nicogarcia <nicoalbo90@gmail.com>
2017-04-26 23:16:52 -0300

    Created custom user manager

--- api/users/users_model.py
+++ api/users/users_model.py
@@ -1,11 +1,15 @@
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
+from api.users.users_manager import UserManager
+
 
 class User(AbstractBaseUser):
     USERNAME_FIELD = 'email'
 
     email = models.EmailField(unique=True)
+
+    objects = UserManager()
 
     def get_short_name(self):
         return self.email
