7a489fd8ba26a07a005b7d437b76146269345625
nicogarcia <nicoalbo90@gmail.com>
2017-04-29 21:14:30 -0300

    Added created_at to User

--- api/users/users_model.py
+++ api/users/users_model.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
+from django.utils import timezone
 
 from api.users.users_manager import UserManager
 
 
 class User(AbstractBaseUser):
     USERNAME_FIELD = 'email'
+
+    created_at = models.DateTimeField(default=timezone.now)
 
     email = models.EmailField(unique=True)
 
